a79e02c36dedb0b981e6cfbcd16b4010cb3a909d
Simon Tse Chi Ming <minixxie@users.noreply.github.com>
2024-02-07 10:55:38 +0800

    Use MongoDB as a choice for index-store and doc-store, by defining Env Var MONGO_URI

--- private_gpt/components/node_store/node_store_component.py
+++ private_gpt/components/node_store/node_store_component.py
@@ -1,8 +1,9 @@
 import logging
+import os
 
 from injector import inject, singleton
-from llama_index.storage.docstore import BaseDocumentStore, SimpleDocumentStore
-from llama_index.storage.index_store import SimpleIndexStore
+from llama_index.storage.docstore import BaseDocumentStore, SimpleDocumentStore, MongoDocumentStore
+from llama_index.storage.index_store import SimpleIndexStore, MongoIndexStore
 from llama_index.storage.index_store.types import BaseIndexStore
 
 from private_gpt.paths import local_data_path
@@ -17,18 +18,22 @@ class NodeStoreComponent:
 
     @inject
     def __init__(self) -> None:
-        try:
-            self.index_store = SimpleIndexStore.from_persist_dir(
-                persist_dir=str(local_data_path)
-            )
-        except FileNotFoundError:
-            logger.debug("Local index store not found, creating a new one")
-            self.index_store = SimpleIndexStore()
+        if os.getenv('MONGO_URI'):
+            self.index_store = MongoIndexStore.from_uri(uri=os.getenv('MONGO_URI'), db_name="private-gpt")
+            self.doc_store = MongoDocumentStore.from_uri(uri=os.getenv('MONGO_URI'), db_name="private-gpt")
+        else:
+            try:
+                self.index_store = SimpleIndexStore.from_persist_dir(
+                    persist_dir=str(local_data_path)
+                )
+            except FileNotFoundError:
+                logger.debug("Local index store not found, creating a new one")
+                self.index_store = SimpleIndexStore()
 
-        try:
-            self.doc_store = SimpleDocumentStore.from_persist_dir(
-                persist_dir=str(local_data_path)
-            )
-        except FileNotFoundError:
-            logger.debug("Local document store not found, creating a new one")
-            self.doc_store = SimpleDocumentStore()
+            try:
+                self.doc_store = SimpleDocumentStore.from_persist_dir(
+                    persist_dir=str(local_data_path)
+                )
+            except FileNotFoundError:
+                logger.debug("Local document store not found, creating a new one")
+                self.doc_store = SimpleDocumentStore()
